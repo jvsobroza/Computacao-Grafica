8fa22ee625b0b285040c089711f2f2068bd21cf1
João Victor Sobroza Dal Ross <joaovictor0908.jv@gmail.com>
2026-09-16 19:24:34 -0300

    aula 09/09

--- Aula09d09/jogoSprite.js
+++ Aula09d09/jogoSprite.js
@@ -2,6 +2,8 @@ class MeuJogo extends JS_CG_2D_API {
   acaoAoIniciar() {
     this.direita = this.carregarFrames("frame_inv", 4);
     this.esquerda = this.carregarFrames("frame", 4);
+    this.esq = false;
+    this.dir = false;
     this.jogador = new Sprite(50, 50, 400, 400);
     this.jogador.setAnimacao(this.direita);
     this.jogador.setVelocidade(30, -4);
@@ -15,6 +17,24 @@ class MeuJogo extends JS_CG_2D_API {
     if (this.jogador.px < 0) {
       this.jogador.setAnimacao(this.direita);
       this.jogador.setVelocidade(30, -10);
+    }
+  }
+
+  teclaPressionada(e) {
+    if (e.key === "ArrowLeft") {
+      this.esq = true;
+      this.jogador.setAnimacao(this.esquerda);
+    }
+    if (e.key === "ArrowRight") {
+      this.dir = true;
+      this.jogador.setAnimacao(this.direita);
+    }
+  }
+
+  teclaLiberada(e) {
+    if (e.key === "ArrowLeft") {
+      this.esq = false;
+      this.jogador.setVelocidade(0,0);
     }
   }
   desenhar() {
